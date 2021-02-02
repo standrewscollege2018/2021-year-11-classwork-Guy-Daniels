0e29be6dc44643488ad1c2ec14bd2cdf1da1a142
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-02-02 13:39:06 +1300

    intro

---
+++ input.py
@@ -0,0 +1,10 @@
+# this program demonstrates storing information in variables
+
+#here is a variable called name is storing the text "Guy"
+name = "Guy"
+
+print("Hello", name)
+
+#here's a variable storing an integer
+number1 = 13
+print(number1 / 2)
---
+++ name.py
@@ -0,0 +1,10 @@
+# this program enables the user to enter their name
+# the program then greets them by name
+
+name = input("what is your name?")
+
+print("Hello", name ," how are you?")
+
+age = input("how old are you?")
+
+print("Hello", name ," and you are" age )

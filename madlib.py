c514ef638054cc6fa748fdd4706605b0b6685049
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-02-02 14:26:27 +1300

    madlib

---
+++ madlib.py
@@ -0,0 +1,36 @@
+# this program is to use madlibs for the first time
+
+# these are the variables to be in the madlib
+
+name = input("Name: ")
+body_part = input("Body part: ")
+illness = input("Illness: ")
+subject = input("Subject: ")
+teacher_name = input("Teacher name: ")
+song = input("Song: ")
+video = input("Video: ")
+activity = input("Activity: ")
+
+# this is the final product
+
+print("Hello", teacher_name ,"I'm sorry that", name ,"cannot get to", subject ,"because their", body_part ,"hurts due to", illness ,)
+print("Instead,", name ,"will be vibing to", song ,"and will be watching", video ,"after he finishes" , activity ,)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

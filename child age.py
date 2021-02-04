7218a036e411910cad5466a3320449724261d704
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-02-04 15:20:18 +1300

    child age

---
+++ child age.py
@@ -0,0 +1,11 @@
+# this program is to see whether the age is young enough for a childs fee
+
+# setting child age and age to enter
+CHILD_AGE = 13
+age = int(input("age: "))
+
+
+if age <= CHILD_AGE:
+    print("you may pay the child price")
+
+print("welcome to the zoo")

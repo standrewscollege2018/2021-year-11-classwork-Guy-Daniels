014b1e095c26a8ca3e8b5a4322a433ad07ace8ec
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-02-05 10:40:38 +1300

    ticket price

---
+++ ticket price.py
@@ -0,0 +1,14 @@
+# this is the ticket price for each age
+
+age = int(input("age: "))
+student = int(input("type 1 if you are a student, 2 if you are not: "))
+
+if age >= 13 and student = 1:
+    print("$8")
+else:
+    if age >= 18:
+        print("$12")
+    elif age >= 13:
+        print("$9")
+    elif age >= 5:
+        print("$7")

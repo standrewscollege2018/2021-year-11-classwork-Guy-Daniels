f0b04fa07ffa2e3b4ef559ef04bcb051f2202b9b
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-02-05 10:17:12 +1300

    if else

--- if else.py
+++ if else.py
@@ -6,11 +6,15 @@ score = int(input("Score: "))
 
 # this is what will be printed depending on the score
 
-if score >= 90:
+if score > 100:
+    print("invalid")
+elif score >= 90:
     print("Excellence")
 elif score >= 70:
     print("Merit")
 elif score >= 50:
     print("Achieved")
+elif score < 0:
+    print("invalid")
 else:
     print("Not achieved")

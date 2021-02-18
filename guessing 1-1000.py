e297ff5ea175971303ca5f2bfe34674d7b3f583f
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-02-18 15:20:12 +1300

    guess game

--- guessing 1-1000.py
+++ guessing 1-1000.py
@@ -5,11 +5,17 @@ HIGH_SCORE = 1000000
 keep_playing = True
 while keep_playing == True:
     score = 0
-    CORRECT_NUMBER = random.randint(1,1000)
+    CORRECT_NUMBER = random.randint(1,1001)
     keep_guessing = True
     while keep_guessing == True:
-        number = int(input("Guess a number between 1 and 1000: "))
         score += 1
+
+        check_number = True:
+            try:
+                guess = int(input("Pick a number between 1-1000"))
+                check_number = false
+            except:
+                print("Please print an integer")
         if number <= 0 or number >= 1001:
             print("invalid")
         else:
@@ -21,6 +27,7 @@ while keep_playing == True:
                 print("Lower")
             elif number < CORRECT_NUMBER:
                 print("Higher")
+            # printing out either lower, higher or within 5
 
             if number == CORRECT_NUMBER:
                 keep_guessing = False
@@ -30,6 +37,8 @@ while keep_playing == True:
     if score < HIGH_SCORE:
         print("New high score is", score ,"!")
         HIGH_SCORE = score
+    #
+
 
     play_again = input("Play again y/n: ")
     if play_again == "n":

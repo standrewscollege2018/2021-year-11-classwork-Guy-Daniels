38d3e384b9be6d66493738686aa53c0a58e1da2c
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-03-02 14:25:18 +1300

    ticket

--- speeding ticket.py
+++ speeding ticket.py
@@ -6,6 +6,8 @@ while keep_asking == True:
     name = input("Name: ")
     if name in (criminals):
         print("You are under arrest")
+    elif name == "end":
+        keep_asking = False
     else:
         limit = int(input("What is the speed limit: "))
         speed = int(input("What speed were you at: "))
@@ -13,5 +15,15 @@ while keep_asking == True:
             print("Have a nice day")
         else:
             amount_over = speed-limit
-            if amount_over < 10:
-                print(name, "")
+            if amount_over < 11:
+                fine = 30
+            elif amount_over < 21:
+                fine = 80
+            elif amount_over <= 29:
+                fine = 130
+            elif amount_over >= 30:
+                fine = 180
+            else:
+                print("Error")
+
+print("You are fined", fine)

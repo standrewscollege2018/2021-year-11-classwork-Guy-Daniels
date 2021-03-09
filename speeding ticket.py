cfa95dc746137b54b05ddb46c2aaf07c0feea60e
Guy-Daniels <78179671+Guy-Daniels@users.noreply.github.com>
2021-03-09 21:31:27 +1300

    speeding

--- speeding ticket.py
+++ speeding ticket.py
@@ -1,7 +1,9 @@
 # this program tells you how much you should be fined
 # depending on how much over the speed limit you were
 criminals = ["Josh McPhail", "Tom Kerry", "Mitchell Corkery"]
+statement = []
 keep_asking = True
+asking_for_speed = True
 while keep_asking == True:
     name = input("Name: ")
     if name in (criminals):
@@ -9,21 +11,28 @@ while keep_asking == True:
     elif name == "end":
         keep_asking = False
     else:
-        limit = int(input("What is the speed limit: "))
-        speed = int(input("What speed were you at: "))
-        if speed < limit+1:
-            print("Have a nice day")
-        else:
-            amount_over = speed-limit
-            if amount_over < 11:
-                fine = 30
-            elif amount_over < 21:
-                fine = 80
-            elif amount_over <= 29:
-                fine = 130
-            elif amount_over >= 30:
-                fine = 180
+        while asking_for_speed == True:
+            try:
+                limit = int(input("What is the speed limit: "))
+                asking_for_speed = False
+            except:
+                print("Invalid answer")
+            speed = int(input("What speed were you at: "))
+            if speed < limit+1:
+                added_statement = (name, "Have a nice day")
             else:
-                print("Error")
+                amount_over = speed-limit
+                if amount_over < 11:
+                    fine = 30
+                elif amount_over < 21:
+                    fine = 80
+                elif amount_over <= 29:
+                    fine = 130
+                elif amount_over >= 30:
+                    fine = 180
+                else:
+                    print("Error")
+                added_statement = (name, "You are fined", fine)
+            statement.append(added_statement)
 
-print("You are fined", fine)
+print(statement)
